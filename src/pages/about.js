d5802ad7dd994fd99c50332d0e22b6b45784f128
Huy Nguyen <huyn.nguyen95@gmail.com>
2021-10-27 16:15:39 -0500

    created layout component, styled the component

--- src/pages/about.js
+++ src/pages/about.js
@@ -2,12 +2,12 @@ import * as React from 'react';
 import { Link } from 'gatsby';
 import Layout from '../components/layout';
 
-const IndexPage = () => {
+function About() {
 	return (
-		<Layout pageTitle="Home Page">
-			<h1>This is the home page</h1>
+		<Layout pageTitle="About Page">
+			<h1>This is the about page</h1>
 		</Layout>
 	);
-};
+}
 
-export default IndexPage;
+export default About;
