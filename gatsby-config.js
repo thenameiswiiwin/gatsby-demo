dab40325e7caaf16203cf017fabef97d996b2ade
Huy Nguyen <huyn.nguyen95@gmail.com>
2021-10-27 16:29:26 -0500

    static image plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,7 @@
 module.exports = {
-  siteMetadata: {
-    siteUrl: "https://www.yourdomain.tld",
-    title: "gatsby-demo",
-  },
-  plugins: [],
+	siteMetadata: {
+		siteUrl: 'https://www.yourdomain.tld',
+		title: 'gatsby-demo',
+	},
+	plugins: ['gatsby-plugin-image', 'gatsby-plugin-sharp'],
 };
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,15 @@
 import * as React from 'react';
-import { Link } from 'gatsby';
 import Layout from '../components/layout';
+import { StaticImage } from 'gatsby-plugin-image';
 
 const IndexPage = () => {
 	return (
 		<Layout pageTitle="Home Page">
 			<h1>This is the home page</h1>
+			<StaticImage
+				alt="Clifford, a reddish-brown pitbull, dozing in a bean bag chair"
+				src="../images/demo.webp"
+			/>
 		</Layout>
 	);
 };
